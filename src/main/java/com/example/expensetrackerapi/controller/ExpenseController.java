34968208ca4aed719eb930bfc4bc66723a7c1b5d
vakser <65089446+vakser@users.noreply.github.com>
2023-09-08 19:48:45 +0300

    Initial commit

---
+++ src/main/java/com/example/expensetrackerapi/controller/ExpenseController.java
@@ -0,0 +1,12 @@
+package com.example.expensetrackerapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+    @GetMapping("/expenses")
+    public String getAllExpenses() {
+        return "List of expenses";
+    }
+}
