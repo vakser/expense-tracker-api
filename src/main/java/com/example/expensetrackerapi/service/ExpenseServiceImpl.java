b9d27a1031be01c2e58e6af3d245f85270c9d193
vakser <65089446+vakser@users.noreply.github.com>
2023-09-12 07:49:23 +0300

    Method deleteExpenseById of ExpenseServiceImpl updated to return correct Http Status 404 in case expense is not found.

--- src/main/java/com/example/expensetrackerapi/service/ExpenseServiceImpl.java
+++ src/main/java/com/example/expensetrackerapi/service/ExpenseServiceImpl.java
@@ -35,7 +35,8 @@ public class ExpenseServiceImpl implements ExpenseService {
 
     @Override
     public void deleteExpenseById(Long id) {
-        expenseRepository.deleteById(id);
+        Expense expense = getExpenseById(id);
+        expenseRepository.delete(expense);
     }
 
     @Override
