c6b1076442e2ae9e3165041543c3124f4585b75a
vakser <65089446+vakser@users.noreply.github.com>
2023-09-14 20:48:28 +0300

    Code updated to validate JWT token.

--- src/main/java/com/example/expensetrackerapi/util/JwtTokenUtil.java
+++ src/main/java/com/example/expensetrackerapi/util/JwtTokenUtil.java
@@ -1,5 +1,6 @@
 package com.example.expensetrackerapi.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtTokenUtil {
@@ -25,5 +27,28 @@ public class JwtTokenUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
                 .signWith(SignatureAlgorithm.HS512, secret)
                 .compact();
+    }
+
+    public String getUsernameFromToken(String jwtToken) {
+        return getClaimFromToken(jwtToken, Claims::getSubject);
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean validateToken(String jwtToken, UserDetails userDetails) {
+        final String username = getUsernameFromToken(jwtToken);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken);
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        final Date expiration = getExpirationDateFromToken(jwtToken);
+        return expiration.before(new Date());
+    }
+
+    private Date getExpirationDateFromToken(String jwtToken) {
+        return getClaimFromToken(jwtToken, Claims::getExpiration);
     }
 }
